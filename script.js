7cd8cf741f6b4c80120f6000d6977ff5ef319585
vickychandra357 <81799094+vickychandra357@users.noreply.github.com>
2023-03-15 05:07:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function getNumbers() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([1, 2, 3, 4]);
+      }, 3000);
+    });
+  }
+getNumbers()
+  .then(numbers => {
+    // Filter out odd numbers
+    const filteredNumbers = numbers.filter(num => num % 2 !== 0);
+    return filteredNumbers;
+  })
+  .then(filteredNumbers => {
+    // Multiply even numbers by 2
+    const multipliedNumbers = filteredNumbers.map(num => num * 2);
+    return multipliedNumbers;
+  })
+  .then(result => {
+    // Update the HTML element with ID "output"
+    const output = document.getElementById("output");
+    output.innerText = result.join(", ");
+  })
+  .catch(error => {
+    console.error(error);
+  });
